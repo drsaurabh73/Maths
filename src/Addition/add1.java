79012f0295616d7354d451bda43bbfd3f259476f
drsaurabh73 <drsaurabh73@gmail.com>
2022-03-11 10:00:54 +0000

    Maths

---
+++ src/Addition/add1.java
@@ -0,0 +1,10 @@
+package Addition;
+
+public class add1 {
+    public static void main (String [] args){
+        int A = 4;
+        int B = 6;
+        int C = 7;
+        System.out.println(A+B+C);
+    }
+}
